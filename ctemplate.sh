4561053854cf4dc3621d176c0cfbf15ceda7966d
joseareia <jose.apareia@gmail.com>
2022-09-26 11:43:29 +0100

    edit: name of the project is now mandatory

--- ctemplate.sh
+++ ctemplate.sh
@@ -9,26 +9,33 @@ show_help() {
 	echo -e "CTemplate - Create new C templates for your projects!"
 	echo -e "Usage: ${0##*/} [-s] [-p PATH]\n"
 	echo -e "\t-h\tshow brief help"
+	echo -e "\t-n\tname of the project"
 	echo -e "\t-s\tcreate a new client-server template (${bold}OPTIONAL${normal})"
 	echo -e "\t-p\tspecify the directory to store the new template (${bold}OPTIONAL${normal})"
 	echo -e "\n${bold}NOTE:${normal} If -p is not used, the template it will be stored in the current directory."
 }
 
 exit_abnormal() {
-	echo -e "Usage: ${0##*/} [-s] [-p PATH]"
+	echo -e "Usage: ${0##*/} [-n NAME] [-s] [-p PATH]"
 }
 
+n_flag=''
+name=''
 s_flag=''
 p_flag=''
 path=''
 
 OPTIND=1
 
-while getopts 'hsp:path' opt; do
+while getopts 'hsn:namep:path' opt; do
 	case $opt in
 		h)
 			show_help
         	exit 0
+			;;
+		n)
+			n_flag='true'
+			name="${OPTARG}"
 			;;
 		s)
 			s_flag='true'
@@ -44,7 +51,13 @@ while getopts 'hsp:path' opt; do
 	esac
 done
 
-echo -e "Copying the necessary files..."
+
+shift $(($OPTIND - 1))
+
+if [ -z "$n_flag" ] && [ -z "$name" ] ; then
+    echo "${0##*/}: option '-n' is required"
+    exit_abnormal >&2 ; exit 1
+fi
 
 if [ ! -z "$p_flag" ] && [ ! -d "$path" ]; then
 	echo -e "The following directory does not exist: $path"
@@ -52,16 +65,22 @@ if [ ! -z "$p_flag" ] && [ ! -d "$path" ]; then
     exit 1
 fi
 
+echo -e "Copying the necessary files..."
+
 if [ ! -z "$s_flag" ]; then
 	if [ ! -z "$p_flag" ]; then
-		cp --preserve $i_path/template_s/* $path
+		mkdir $path/$name
+		cp --preserve -r $i_path/template_s/* $path/$name
 	fi
-	cp --preserve $i_path/template_s/* $PWD
+	mkdir $PWD/$name
+	cp --preserve -r $i_path/template_s/* $PWD/$name
 else
 	if [ ! -z "$p_flag" ]; then
-		cp --preserve $i_path/template/* $path
+		mkdir $path/$name
+		cp --preserve -r $i_path/template/* $path/$name
 	fi
-	cp --preserve $i_path/template/* $PWD
+	mkdir $PWD/$name
+	cp --preserve -r $i_path/template/* $PWD/$name
 fi
 
 shift "$((OPTIND-1))"
